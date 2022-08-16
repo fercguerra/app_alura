3deb22d8fc533fd301d18f7d9a2767668a194c4b
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-08-16 12:58:12 -0300

    Image

--- lib/main.dart
+++ lib/main.dart
@@ -23,14 +23,16 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            const Task('Aprender Flutter comendo sucrilhos no café da manhã'),
-            const Task('Andar de Bike'),
-            const Task('Meditar'),
-            const Task('Meditar'),
-            const Task('Meditar'),
-            const Task('Meditar'),
-            const Task('Meditar'),
-            const Task('Meditar'),
+            const Task('Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            const Task('Andar de Bike',
+                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg'),
+            const Task('Meditar',
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            const Task('Ler',
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
+            const Task('Jogar',
+                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg'),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -43,7 +45,8 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String nome;
-  const Task(this.nome, {Key? key}) : super(key: key);
+  final String foto;
+  const Task(this.nome, this.foto, {Key? key}) : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
@@ -74,6 +77,10 @@ class _TaskState extends State<Task> {
                         color: Colors.black26,
                         width: 72,
                         height: 100,
+                        child: Image.network(
+                          widget.foto,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                       Container(
                         width: 200,
